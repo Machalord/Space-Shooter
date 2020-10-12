6e273c7de0562acaea2c149e94bf44a915d506c7
Machalord <m.machado.dev@gmail.com>
2020-10-12 18:57:53 -0300

    Path Follow Testing

---
+++ Scripts/EnemyPathFormation.gd
@@ -0,0 +1,20 @@
+extends Path2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$PathFollow2D.offset+=600*delta
+	$PathFollow2D2.offset+=600*delta
+	$PathFollow2D3.offset+=600*delta
+	$PathFollow2D4.offset+=600*delta
+	pass
